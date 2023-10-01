f61a9c9b93c09ac6e19accaa418c3c3c19102189
zaw8300 <mohamed.zaouali15@gmail.com>
2023-10-01 23:51:25 +0100

    Atelier_3

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -1,50 +1,34 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
- class AuthorController extends AbstractController
- {
-     #[Route('/author', name: 'app_author')]
-     public function index(): Response
-     {
-         return $this->render('author/list.html.twig', [
-             'controller_name' => 'AuthorController',
-         ]);
-     }
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
 
-     #[Route("/author/{name}", name: "author_list")]
-     public function author_list(): void
-     {
-         $authors = [
-             [
-                 'id' => 1,
-                 'picture' => '/images/Victor-Hugo.jpg',
-                 'username' => 'Victor Hugo',
-                 'email' => 'victor.hugo@gmail.com',
-                 'nb_books' => 100,
-             ],
-             [
-                 'id' => 2,
-                 'picture' => '/images/william-shakespeare.jpg',
-                 'username' => 'William Shakespeare',
-                 'email' => 'william.shakespeare@gmail.com',
-                 'nb_books' => 200,
-             ],
-             [
-                 'id' => 3,
-                 'picture' => '/images/Taha_Hussein.jpg',
-                 'username' => 'Taha Hussein',
-                 'email' => 'taha.hussein@gmail.com',
-                 'nb_books' => 300,
-             ],
-         ];
-         if (empty($authors)) {
-             $authors = [];
-         }
-     }
+    #[Route('/listAuthor',name:'listAuthor')]
+    public function list() : Response
+    {
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg', 'username' => 'Victor Hugo','email' => 'victor.hugo@gmail.com','nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg', 'username' => 'William Shakespeare','email' => 'william.shakespeare@gmail.com','nb_books' => 200),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg', 'username' => 'Taha Hussein','email' => 'taha.hussein@gmail.com','nb_books' => 300),
+        );
+        // $authors = array();
+        return $this->render('author/list.html.twig',[
+            'authors' => $authors,
+        ]);
+    }
 
      #[Route("/author/details/{id}", name: "author_details")]
      public function authorDetails($id, $authors)
@@ -54,5 +38,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
      }
 
- }
+}
+
+
 
